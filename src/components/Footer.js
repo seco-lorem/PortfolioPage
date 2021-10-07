5cd856512b190e94dae164043f46bc9f9dd7c41d
seco-lorem <62249520+seco-lorem@users.noreply.github.com>
2021-10-07 23:03:39 +0300

    updated page contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,8 +10,9 @@ function Footer() {
             <div className="footer"/>
             <div className="nav-bar">
                 <span className="disabled-button"> Designed by industrial design student </span>
-                <a className="button doga-button" target="_blank" href={"https://dogaozyurtid.wordpress.com/"}> Doğa </a>
-                <span className="disabled-button"> , CSS and reactJS implemented by Me. </span>
+                <a className="button credit-button" target="_blank" href={"https://dogaozyurtid.wordpress.com/"}> Doğa </a>
+                <span className="disabled-button"> , CSS and reactJS implemented by </span>
+                <a className="button credit-button" target="_blank" href={"https://yektaseckinsatir.com/"}> Me </a>
             </div>
         </div>
     );
