76f57110471b1a698b879bebf220f66516f3cd28
seco-lorem <62249520+seco-lorem@users.noreply.github.com>
2022-01-18 19:27:18 +0300

    updated links page style

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -1,32 +1,50 @@
 import '../stylesheets/Pages.css';
 import '../stylesheets/Header.css';
 import data from '../pageContents';
-
+import React, { useState, useRef} from 'react';
 
 const skills = data.skills;
 const explanation = data.skillsExplanation;
 
 function SkillsPage() {
+/*
+    const [btnClassName, setBtnClassName] = useState("");
+
+    const indexRef = useRef(window.innerHeight);
+    indexRef.current = window.innerHeight;
+    const a = () => {
+        console.log(indexRef.current);
+        if (indexRef.current < 800) {
+            setBtnClassName("small skill");
+        }
+        else {
+            setBtnClassName("skill");
+        }
+    }
+    window.onresize = a;
+    console.log("Rerender");*/
     return (
         <div>
             <h1> Skills </h1>
             <p>{explanation}</p>
-            {skills.map((skill) => {
-                return (
-                    <div className="skill">
-                        <button className="skill-title box"> {skill.name}</button>
-                        <div className="skill-detail">
-                            {skill.explanation}
-                            <div/>
-                            {skill.examples.map((example) => {
-                                return(
-                                    <a className="link box" target="_blank" href={example.link}>{example.title}</a>
-                                )
-                            })}
+            <div className="center" >
+                {skills.map((skill) => {
+                    return (
+                        <div className="skill">
+                            <button className="skill-title box"> {skill.name}</button>
+                            <div className="skill-detail">
+                                {skill.explanation}
+                                <div/>
+                                {skill.examples.map((example) => {
+                                    return(
+                                        <a className="link box" target="_blank" href={example.link}>{example.title}</a>
+                                    )
+                                })}
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     );
 }
